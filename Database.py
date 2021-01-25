832012a1e36981b5cad4cf38c17a6e443a5e854b
yura159 <yuriy.ivanov.01@bk.ru>
2021-01-25 20:47:18 +0500

    БД
    Добавил таблицу типов работ. Методы добовления и получения все данных. Полная отчитска таблицы

--- Database.py
+++ Database.py
@@ -44,6 +44,39 @@ class Database:
         data = [(my_order.Number, my_order.Price, my_order.Time, my_order.Type)]
         Database._AddElement('orders.db', request, data)
 
+    @staticmethod
+    def AddTypeWork(data):
+        request = 'insert into WORK_TYPE values (\'' + data + '\')'
+        work = sl.connect('work_type.db')
+        with work:
+            work.execute(request)
 
-x = Database()
-print(x.Find(1))
+    @staticmethod
+    def GetTypeWork():
+        request = 'select * from WORK_TYPE'
+        work = sl.connect('work_type.db')
+        result = []
+        with work:
+            for val in work.execute(request):
+                result.append(val)
+        return result
+
+    @staticmethod
+    def DeleteData(path, name_database):
+        database = sl.connect(path)
+        cursor = database.cursor()
+        request = """delete from """ + name_database
+        cursor.execute(request)
+        database.commit()
+
+
+y = Database()
+y.AddTypeWork("Чистка")
+for val in y.GetTypeWork():
+    print(val)
+y.DeleteData('work_type.db', "WORK_TYPE")
+print('Удаление произошло?')
+for val in y.GetTypeWork():
+    print(val)
+
+print('Произошло :)')
