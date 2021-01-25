961c16f0718e5e0939e1d11a8b475f1a6d1fd792
yura159 <yuriy.ivanov.01@bk.ru>
2021-01-25 19:38:39 +0500

    БД
    Добавил поиск всех данных по номеру. Добавление данных в таблицу клиентов.

--- Database.py
+++ Database.py
@@ -2,8 +2,7 @@ import sqlite3 as sl
 
 
 class DataOrders:
-    def __init__(self, id, number, price, time, type_orders, name_customer, car):
-        self.Id = id
+    def __init__(self, number, price, time, type_orders, name_customer, car):
         self.Number = number
         self.Price = price
         self.Time = time
@@ -13,18 +12,38 @@ class DataOrders:
 
 
 class Database:
-    customer = sl.connect('customer.db')
 
-    def AddOrders(self, my_order):
+    @staticmethod
+    def _AddElement(path, request, data):
+        database = sl.connect(path)
+        with database:
+            database.executemany(request, data)
+
+    @staticmethod
+    def AddCustomer(my_customer):
+        request = 'insert into CUSTOMERS (Number, Name, Car) values (?, ?, ?)'
+        data = [(my_customer.Number, my_customer.Name, my_customer.Car)]
+        Database._AddElement('customer.db', request, data)
+
+    @staticmethod
+    def Find(number):
+        customers = sl.connect('customer.db')
         orders = sl.connect('orders.db')
-        request = 'insert into ORDERS (ID, Number, Price, Time, Type) values (?, ?, ?, ?, ?)'
-        data = [(my_order.Id, my_order.Number, my_order.Price, my_order.Time, my_order.Type)]
+        result = []
+        request = 'select * from ORDERS where Number = ' + str(number)
         with orders:
-            orders.executemany(request, data)
+            result += orders.execute(request)
+        request = 'select * from CUSTOMERS where Number = ' + str(number)
+        with customers:
+            result += customers.execute(request)
+        return result
+
+    @staticmethod
+    def AddOrders(my_order):
+        request = 'insert into ORDERS (Number, Price, Time, Type) values (?, ?, ?, ?)'
+        data = [(my_order.Number, my_order.Price, my_order.Time, my_order.Type)]
+        Database._AddElement('orders.db', request, data)
 
 
-c = sl.connect('orders.db')
-with c:
-    database = c.execute('select * from ORDERS')
-    for row in database:
-        print(row)
+x = Database()
+print(x.Find(1))
