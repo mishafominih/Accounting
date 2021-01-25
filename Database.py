fe7f4c6c6952797944203da85ece84d5476083a4
mishafominih <mishagjxn@mail.ru>
2021-01-25 20:26:08 +0500

    Revert "Merge branch 'main' of https://github.com/mishafominih/Accounting into main"
    This reverts commit cba35afc61994a4dd41501f026e125d2dda799ed, reversing
    changes made to e9bdb95cb81849562b80d33a4727e4c803858460.

--- Database.py
+++ Database.py
@@ -2,7 +2,8 @@ import sqlite3 as sl
 
 
 class DataOrders:
-    def __init__(self, number, price, time, type_orders, name_customer, car):
+    def __init__(self, id, number, price, time, type_orders, name_customer, car):
+        self.Id = id
         self.Number = number
         self.Price = price
         self.Time = time
@@ -12,38 +13,18 @@ class DataOrders:
 
 
 class Database:
+    customer = sl.connect('customer.db')
 
-    @staticmethod
-    def _AddElement(path, request, data):
-        database = sl.connect(path)
-        with database:
-            database.executemany(request, data)
-
-    @staticmethod
-    def AddCustomer(my_customer):
-        request = 'insert into CUSTOMERS (Number, Name, Car) values (?, ?, ?)'
-        data = [(my_customer.Number, my_customer.Name, my_customer.Car)]
-        Database._AddElement('customer.db', request, data)
-
-    @staticmethod
-    def Find(number):
-        customers = sl.connect('customer.db')
+    def AddOrders(self, my_order):
         orders = sl.connect('orders.db')
-        result = []
-        request = 'select * from ORDERS where Number = ' + str(number)
+        request = 'insert into ORDERS (ID, Number, Price, Time, Type) values (?, ?, ?, ?, ?)'
+        data = [(my_order.Id, my_order.Number, my_order.Price, my_order.Time, my_order.Type)]
         with orders:
-            result += orders.execute(request)
-        request = 'select * from CUSTOMERS where Number = ' + str(number)
-        with customers:
-            result += customers.execute(request)
-        return result
-
-    @staticmethod
-    def AddOrders(my_order):
-        request = 'insert into ORDERS (Number, Price, Time, Type) values (?, ?, ?, ?)'
-        data = [(my_order.Number, my_order.Price, my_order.Time, my_order.Type)]
-        Database._AddElement('orders.db', request, data)
+            orders.executemany(request, data)
 
 
-x = Database()
-print(x.Find(1))
+c = sl.connect('orders.db')
+with c:
+    database = c.execute('select * from ORDERS')
+    for row in database:
+        print(row)
